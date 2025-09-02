21926e18786108dc8ba8649b6a734b03a97f36d9
kinshieslayer <kinkinshie@gmail.com>
2025-09-02 21:41:00 +0100

    J'ai fait l'exercice 8 des boucles, suite Fibonacci

---
+++ day01/boucles/challenge8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, a = 0, b = 1, suivant;
+    printf("Entrez le nombre de termes : ");
+    scanf("%d", &n);
+
+    printf("Suite de Fibonacci : ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a);
+        suivant = a + b;
+        a = b;
+        b = suivant;
+    }
+    printf("\n");
+    return 0;
+}
